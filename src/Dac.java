f5393b56e84424d44fb6eb3a5181013a78039e81
prathama pande <prathamapande@gmail.com>
2022-08-11 19:08:07 +0530

    hi guys job done

---
+++ src/Dac.java
@@ -0,0 +1,11 @@
+
+public class Dac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hi class this project created by lead ");
+		
+	}
+
+}
